c28232360e23944ff955e0932006254ea96b9e6b
Asilbec <asilbekomonkulov2003@gmail.com>
2021-10-26 23:14:08 -0400

    fixed the issues

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
           <p>
             {"\t"} Kanye Omari West (born June 8, 1977) is an American rapper, singer, songwriter, record producer,
             entrepreneur and fashion designer. His musical career has been marked by dramatic changes in styles,
-            incorporating an eclectic range of influences including soul, baroque pop, electro, indie rock, synth-pop, industrial and gospel. Over the course of his career, West has been responsible for cultural movements and progressions within mainstream hip hop and popular music at large.
-
+            incorporating an eclectic range of influences including soul, baroque pop, electro, indie rock, synth-pop,
+            industrial and gospel. Over the course of his career, West has been responsible for cultural movements and progressions within mainstream hip hop and popular music at large.
             Born in Atlanta and raised in Chicago, West first became known as a producer for Roc-A-Fella Records in
             the early 2000s, producing hit singles for recording artists such as Jay-Z, Ludacris and Alicia Keys. Intent
             on pursuing a solo career as a rapper, West released his debut album The College Dropout in 2004 to widespread
@@ -27,7 +27,43 @@ function App() {
             Cudi respectively.</p>
         </div>
       </div>
-      <div className='albums'></div>
+      <div className='albums'>
+        <h1 id='nice'>Favorite Albums: </h1>
+        <div id='order' className='section'>
+          <div className='pic' id='myDark'></div>
+          <div className='top' id='myDarkText'>
+            <h1>My Beautiful Dark Twisted Fantasy </h1>
+            <h1>1.Gorgeous</h1>
+            <h1>2.Devil In A New Dress</h1>
+            <h1>3.POWER</h1>
+            <h1>4.Runaway</h1>
+            <h1>5.All Of The Lights</h1>
+          </div>
+        </div>
+        <div id='order' className='section'>
+          <div className='pic' id='grad'></div>
+          <div className='top' id='gradtext'>
+            <h1>Graduation </h1>
+            <h1>1.Homecoming</h1>
+            <h1>2.Flashing Lights</h1>
+            <h1>3.Champion</h1>
+            <h1>4.I Wonder</h1>
+            <h1>5.Good Morning</h1>
+          </div>
+        </div>
+        <div id='order' className='section'>
+          <div className='pic' id='late'></div>
+          <div className='top' id='latetext'>
+            <h1>TOP 5 SONGS : </h1>
+            <h1>1.Gorgeous</h1>
+            <h1>2.Devil In A New Dress</h1>
+            <h1>3.POWER</h1>
+            <h1>4.Runaway</h1>
+            <h1>5.All Of The Lights</h1>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
